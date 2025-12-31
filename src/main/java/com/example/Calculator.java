9ecbcb51a5d22b13f8724b9922bfe773e32a97ce
solkim7612 <kssol626@gmail.com>
2025-12-31 09:03:41 +0000

    update to Canary Red Version

--- src/main/java/com/example/Calculator.java
+++ src/main/java/com/example/Calculator.java
@@ -27,7 +27,7 @@ public class Calculator {
     static class MyHandler implements HttpHandler {
         @Override
         public void handle(HttpExchange t) throws IOException {
-	    String response = "<h1 style='color:blue'>OLD Version (Blue)</h1><p>Stable Release</p>";
+	    String response = "<h1 style='color:red'>CANARY Version (Red)</h1><p>Testing...</p>";
 
             t.sendResponseHeaders(200, response.length());
             OutputStream os = t.getResponseBody();
